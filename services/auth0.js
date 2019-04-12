67628ef08dd250e1996f289c60d504788ea067ef
Nicholas Wease <skynet60@outlook.com>
2019-04-12 10:18:17 +0200

    Worked on cookies and authentication further.

--- services/auth0.js
+++ services/auth0.js
@@ -1,18 +1,21 @@
 import auth0 from 'auth0-js';
+import Cookies from 'js-cookie';
 
 class Auth0 {
 
     constructor(){
         this.auth0 = new auth0.WebAuth({
             domain: 'nwease.auth0.com',
-            clientID: 'qEdfwcAEmdG0xMi009Ot4egA5YaUZFWe',
+            clientID: 'tjamzxX55eSXdMqo03tpoe7yFjBUWzQN',
             redirectUri: 'http://localhost:3000/callback',
             responseType: 'token id_token',
             scope: 'openid profile'
         });
 
         this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
         this.handleAuthentication = this.handleAuthentication.bind(this);
+        this.isAuthenticated = this.isAuthenticated.bind(this);
     }
 
     handleAuthentication() {
@@ -30,12 +33,40 @@ class Auth0 {
         });
     }
 
-    setSession = () => {
-        // SAVE TOKENS!!!!
-    };
+    setSession(authResult) {
+        // Set isLoggedIn flag in localStorage
+        localStorage.setItem('isLoggedIn', 'true');
+
+        // Set the time that the access token will expire at
+        const expiresAt = (authResult.expiresIn * 1000) + new Date().getTime();
+
+        // this.accessToken = authResult.accessToken;
+
+        Cookies.set('user', authResult.idTokenPayload);
+        Cookies.set('jwt', authResult.idToken);
+        Cookies.set('expiresAt', expiresAt);
+    }
+
+    logout() {
+        Cookies.remove('user');
+        Cookies.remove('jwt');
+        Cookies.remove('expiresAt');
+
+        this.auth0.logout({
+            returnTo: '',
+            clientId: 'tjamzxX55eSXdMqo03tpoe7yFjBUWzQN'
+        })
+    }
 
     login() {
         this.auth0.authorize();
+    }
+
+    isAuthenticated() {
+        // Check whether the current time is past the
+        // access token's expiry time
+        const expiresAt = Cookies.getJson('expiresAt');
+        return new Date().getTime() < expiresAt;
     }
 }
 
