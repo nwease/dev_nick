209c3268b0c3efd43fcaebce51be85d93728b875
Nicholas Wease <skynet60@outlook.com>
2019-04-13 23:10:12 +0200

    servers, secretData, etc...

--- pages/secret.js
+++ pages/secret.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Layout from '../components/layout/Layout';
 import BasePage from '../components/basePage';
 import withAuth from '../components/hoc/withAuth';
+import axios from 'axios';
 
 class Secret extends Component {
 
@@ -9,6 +10,44 @@ class Secret extends Component {
         const protectedSecret = 'Protected Secret';
 
         return {protectedSecret}
+    }
+
+    // constructor(props){
+    //     super(props);
+    //
+    //     this.state = {
+    //         secretData: []
+    //     }
+    // }
+
+    state = {
+        secretData: []
+    };
+
+    async componentDidMount() {
+        const res = await axios.get('/api/v1/secret');
+        const secretData = res.data;
+
+        this.setState({
+            secretData
+        });
+    }
+
+    displaySecretData(){
+        const {secretData} = this.state;
+
+        if (secretData && secretData.login > 0) {
+            return secretData.map((data, index) => {
+                return (
+                    <div key={index}>
+                        <p>{data.title}</p>
+                        <p>{data.description}</p>
+                    </div>
+                )
+            })
+        }
+
+        return null;
     }
 
     render() {
@@ -21,6 +60,7 @@ class Secret extends Component {
                     <h1>Protected</h1>
                     <p>Protected Content Here</p>
                     <h2>{protectedSecret}</h2>
+                    {this.displaySecretData()}
                 </BasePage>
             </Layout>
         )
