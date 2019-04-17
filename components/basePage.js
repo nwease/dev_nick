0ba88e6628a585d5493c454ea7ed87c86f2502d9
Nicholas Wease <skynet60@outlook.com>
2019-04-17 10:58:51 +0200

    Added cards from reactstrap.

--- components/basePage.js
+++ components/basePage.js
@@ -10,7 +10,12 @@ const BasePage = (props) => {
     return (
         <div className={`base-page ${className}`}>
             <Container>
-                {title && <div className='page-header'><h1 className='page-header-title'>{title}</h1></div>}
+
+                {title &&
+                <div className='page-header'>
+                    <h1 className='page-header-title'>{title}</h1>
+                </div>}
+
                 {props.children}
             </Container>
         </div>
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import Layout from '../components/layout/Layout';
 import {Link} from '../routes';
-import BasePage from "../components/basePage";
+import BasePage from '../components/basePage';
+import { Card, Col, Row, CardText, CardBody,
+    CardTitle, CardHeader } from 'reactstrap';
 
 class Portfolio extends Component {
 
@@ -22,11 +24,21 @@ class Portfolio extends Component {
     renderPosts = (posts) => {
         return posts.map((post, index) => {
             return (
-                <li key={index}>
-                    <Link route={`/details/${post.id}`}>
-                        <a> {post.title} </a>
-                    </Link>
-                </li>
+                <Col md='4'>
+                    <React.Fragment key={index}>
+                        <span>
+                            <Card className='portfolio-card'>
+                                <CardHeader className='portfolio-card-header'>Some Position {index}</CardHeader>
+                                <CardBody>
+                                    <p className='portfolio-card-city'> Some Location {index} </p>
+                                    <CardTitle className='portfolio-card-title'>Some Company {index}</CardTitle>
+                                    <CardText className='portfolio-card-text'>Some Description {index}</CardText>
+                                    <div className='readMore'> </div>
+                                </CardBody>
+                            </Card>
+                        </span>
+                    </React.Fragment>
+                </Col>
             )
         })
     };
@@ -37,10 +49,10 @@ class Portfolio extends Component {
 
         return (
            <Layout {...this.props.auth}>
-               <BasePage title='Portfolio'>
-                   <ul>
+               <BasePage className='portfolio-page' title='Portfolio'>
+                   <Row>
                        {this.renderPosts(posts)}
-                   </ul>
+                   </Row>
                </BasePage>
            </Layout>
         );
